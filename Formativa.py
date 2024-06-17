2b6749aefced83aec7e32d0445cefa2a9227d311
bbenjaaa18 <benjamella926@gmail.com>
2024-06-17 12:38:09 -0400

    Listado trabajadores (prueba)

---
+++ Formativa.py
@@ -0,0 +1,16 @@
+"""
+#Listar todos los trabajadores
+lista1 = ["Trabajador","Cargo","Sueldo Bruto","Desc. Salud", "Desc. AFP", "Líquido a pagar"]
+print(lista1)
+lista = [["Matias","CEO","1000000","70000","120000","810000"]
+         ,["Jorge","CEO","1200000","120000","100000","900000"]
+         ,["Ignacio","CEO","1150000","75000","120000","810000"]
+         ,["Juan","CEO","1000000","7000","120000","810000"]
+         ,["Joaquin","CEO","1000000","70000","120000","810000"]
+         ,["Manuel","CEO","1200000","120000","100000","900000"]
+         ,["Javiera","CEO","1150000","75000","120000","810000"]
+         ,["Francisco","CEO","1000000","7000","120000","810000"]]
+print(lista)
+
+
+"""
